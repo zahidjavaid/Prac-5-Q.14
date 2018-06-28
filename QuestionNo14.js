1cf3e9cae3de5356a5823c82d2ee48db0231ab53
zahidjavaid <39004100+zahidjavaid@users.noreply.github.com>
2018-06-28 11:59:43 +0500

    Add files via upload

---
+++ QuestionNo14.js
@@ -0,0 +1,52 @@
+// 5.14 (Challenge!)
+// Write a program that prints the following using a for loop:
+
+// 11 ~ 12 ~ 13 ~ 14 ~ 15
+// 21 ~ 22 ~ 23 ~ 24 ~ 25
+// 31 ~ 32 ~ 33 ~ 34 ~ 35
+// 41 ~ 42 ~ 43 ~ 44 ~ 45
+// 51 ~ 52 ~ 53 ~ 54 ~ 55
+// 61 ~ 62 ~ 63 ~ 64 ~ 65
+// 71 ~ 72 ~ 73 ~ 74 ~ 75
+// 81 ~ 82 ~ 83 ~ 84 ~ 85
+
+let sentence1 = "";
+let sentence2 = "";
+let sentence3 = "";
+let sentence4 = "";
+let sentence5 = "";
+let sentence6 = "";
+let sentence7 = "";
+let sentence8 = "";
+for(let numb1=11; numb1<=15; numb1++){
+	sentence1 = sentence1 + "" + numb1 + " ~ ";
+	}
+	console.log(sentence1);
+	for(let numb2=21; numb2<=25; numb2++){
+		sentence2 = sentence2 + "" + numb2 + " ~ ";
+	}
+	console.log(sentence2);
+		for(let numb3=31; numb3<=35; numb3++){
+			sentence3 = sentence3 + "" + numb3 + " ~ ";
+		}
+		console.log(sentence3);
+		for(let numb4=41; numb4<=45; numb4++){
+			sentence4 = sentence4 + "" + numb4 + " ~ ";
+		}
+		console.log(sentence4);
+		for(let numb5=51; numb5<=55; numb5++){
+			sentence5 = sentence5 + "" + numb5 + " ~ ";
+		}
+		console.log(sentence5);
+		for(let numb6=61; numb6<=65; numb6++){
+			sentence6 = sentence6 + "" + numb6 + " ~ ";
+		}
+		console.log(sentence6);
+		for(let numb7=71; numb7<=75; numb7++){
+			sentence7 = sentence7 + "" + numb7 + " ~ ";
+		}
+		console.log(sentence7);
+		for(let numb8=81; numb8<=85; numb8++){
+			sentence8 = sentence8 + "" + numb8 + " ~ ";
+		}
+		console.log(sentence8);
